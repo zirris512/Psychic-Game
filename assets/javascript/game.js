c69f341c3ea41d0b23bfa086ea3a4349db3b502f
Brent <mina512@live.com>
2019-09-20 17:50:14 -0500

    Added array duplicate checker and if else statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,46 @@
 var userText = "";
-var cpuGuess = "";
+var textArr = [];
+var cpuText = "";
 var wins = 0;
 var loss = 0;
-var guessLeft = 0;
+var guessLeft = 10;
 
 function cpuGuess() {
-var result = "";
 var characters = " abcdefghijklmnopqrstuvwxyz";
-result = characters.charAt(Math.floor((Math.random()*26) + 1));
-return result;
-};
+cpuText = characters.charAt(Math.floor((Math.random()*26) + 1));
 
+};
+cpuGuess();
+console.log(cpuText);
 
 document.onkeyup = function(guess) {
     userText = guess.key;
+    textArr.push(userText);
+    for (var i = 0; i < textArr.length - 1; i++) {
+        var j = textArr[textArr.length - 1];
+        console.log(j);
+        if (j === textArr[i]) {
+            textArr.pop();
+            return;
+        }
+    }        
+    console.log(textArr);
+    console.log(userText);
+    if (userText === cpuText) {
+        wins++;
+        document.querySelector("#user-wins").innerHTML = wins;
+    }
+    else {
+        if (guessLeft > 0) {
+            document.querySelector("#user-guess").innerHTML = textArr.toString();
+            guessLeft--;
+            document.querySelector("#num-guess").innerHTML = guessLeft;
+        }
+        else {
+            loss++;
+            document.querySelector("#user-loss").innerHTML = loss;
+            textArr = [];
+            cpuGuess();
+        }
+    }
 };
-
