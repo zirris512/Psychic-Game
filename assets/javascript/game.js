b86e4205d5d0ae48afa1416c36c7edca55cc0839
Brent <mina512@live.com>
2019-09-20 16:00:59 -0500

    Updated js for key listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,18 @@
+var userText = "";
+var cpuGuess = "";
+var wins = 0;
+var loss = 0;
+var guessLeft = 0;
+
+function cpuGuess() {
 var result = "";
-var characters = "abcdefghijklmnopqrstuvwxyz";
-result += characters.charAt(Math.floor((Math.random()*25) + 1));
-var userText = document.getElementById("user-guess");
-console.log(result);
+var characters = " abcdefghijklmnopqrstuvwxyz";
+result = characters.charAt(Math.floor((Math.random()*26) + 1));
+return result;
+};
+
+
+document.onkeyup = function(guess) {
+    userText = guess.key;
+};
+
